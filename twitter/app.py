5e816e7a8990ebce9cdadff2975a5346b3ac567b
Stephanie Tang <61029209+stang912@users.noreply.github.com>
2020-08-11 22:46:03 -0700

    updated with flask

---
+++ twitter/app.py
@@ -0,0 +1,40 @@
+import os
+import numpy as np
+import flask
+import pickle
+from flask import Flask, render_template, request
+#from model import tweet_predict
+#import model
+
+#creating instance of the class
+app=Flask(__name__)
+
+#run the model while starting flask
+#model.model_start()
+
+
+#def ValuePredictor(tweet):
+    #result = model.tweet_predict(tweet)
+    #return result
+
+#to tell flask what url shoud trigger the function index()
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/visuals.html')
+def about():
+    return flask.render_template('visuals.html')
+
+
+#@app.route('/result.html',methods = ['POST'])
+#def result():
+    #if request.method == 'POST':
+        #to_predict_list = request.form.to_dict()
+        #result = ValuePredictor(to_predict_list)
+        #return render_template("result.html", prediction=result)
+
+    #return 
+    
+if __name__ == "__main__":
+    app.run(debug=True)
