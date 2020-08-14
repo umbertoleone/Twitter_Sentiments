5caa934ca3fda976ff6fb7cccd79851ffc289968
Stephanie Tang <61029209+stang912@users.noreply.github.com>
2020-08-13 19:36:32 -0700

    update files

--- twitter/app.py
+++ twitter/app.py
@@ -3,19 +3,19 @@ import numpy as np
 import flask
 import pickle
 from flask import Flask, render_template, request
-#from model import tweet_predict
-#import model
+from nbtfidf import tweet_predict
+import nbtfidf
 
 #creating instance of the class
 app=Flask(__name__)
 
 #run the model while starting flask
-#model.model_start()
+nbtfidf.model_start()
 
 
-#def ValuePredictor(tweet):
-    #result = model.tweet_predict(tweet)
-    #return result
+def ValuePredictor(tweet):
+    result = nbtfidf.tweet_predict(tweet)
+    return result
 
 #to tell flask what url shoud trigger the function index()
 @app.route('/')
@@ -27,14 +27,14 @@ def about():
     return flask.render_template('visuals.html')
 
 
-#@app.route('/result.html',methods = ['POST'])
-#def result():
-    #if request.method == 'POST':
-        #to_predict_list = request.form.to_dict()
-        #result = ValuePredictor(to_predict_list)
-        #return render_template("result.html", prediction=result)
+@app.route('/result.html',methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        result = ValuePredictor(to_predict_list)
+        return render_template("result.html", prediction=result)
 
-    #return 
+    return 
     
 if __name__ == "__main__":
     app.run(debug=True)
--- twitter/nbtfidf.py
+++ twitter/nbtfidf.py
@@ -5,8 +5,8 @@ import pandas as pd
 import re
 import nltk
 #comment out these two after the first run
-# nltk.download('stopwords')
-# nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from string import punctuation 
@@ -18,7 +18,7 @@ from sklearn.feature_extraction.text import CountVectorizer # for extracting BOW
 from sklearn.feature_extraction.text import TfidfVectorizer # for extracting TDIDF features 
 
 from sklearn.model_selection import train_test_split #for cross validation of train dataset
-from sklearn.ensemble import RandomForestClassifier #classifier
+from sklearn.naive_bayes import GaussianNB #classifier
 from sklearn.metrics import f1_score, confusion_matrix, classification_report
 
 import joblib # for saving our model
@@ -28,7 +28,7 @@ def model_start():
 
     #Read data files
     traindf = pd.read_csv('data/train.csv')
-    #print (traindf)
+    # print (traindf)
     testdf = pd.read_csv('data/test.csv')
     #drop duplicates
     traindf.drop_duplicates(inplace=True)
@@ -48,28 +48,11 @@ def model_start():
         tweet = [ps.stem(word) for word in tweet if not word in set(all_stopwords)]
         tweet = ' '.join(tweet)
         corpus.append(tweet)
-        #clean testdf tweets
-    # corpus_test = []
-    # # loop thru our train dataset 
-    # for i in range (0, len(testdf)):
-    #     tweet = testdf['tweet'][i]
-    #     tweet = tweet.lower()
-    #     tweet = re.sub('[^a-zA-Z]', ' ', tweet) 
-    #     tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet) 
-    #     tweet = re.sub('@[^\s]+', 'AT_USER',  tweet) 
-    #     tweet = re.sub(r'#([^\s]+)', r'\1', tweet) # remove the # in #hashtag
-    #     tweet = tweet.split()
-    #     ps = PorterStemmer()
-    #     all_stopwords = stopwords.words('english')+ list(punctuation) + ['AT_USER','URL', 'user']
-    #     tweet = [ps.stem(word) for word in tweet if not word in set(all_stopwords)]
-    #     tweet = ' '.join(tweet)
-    #     corpus_test.append(tweet)
+        
     #assign corpus to each df and rename columns
     traindf['cleaned'] = np.array(corpus)
     train = traindf.drop(columns=['id', 'tweet'])
-    # testdf['cleaned'] = np.array(corpus_test)
-    # test = testdf.drop(columns=['id', 'tweet'])
-
+    
     #upsampling minority class of Train dataset
     #rename dfs with majority(non_hate) and nimority(hate)
     train_majority = train[train['label'] == 0]
@@ -83,24 +66,21 @@ def model_start():
     #Extract Features
     tfidf_vectorizer = TfidfVectorizer(max_features=1000)
     train_upsampled_tfidf = tfidf_vectorizer.fit_transform(train_upsampled['cleaned']).toarray()
-    # test_tfidf = tfidf_vectorizer.fit_transform(test['cleaned']).toarray()
-    #cross validate train set
-    X_train_tfidf, X_val_tfidf, y_train, y_val = train_test_split(train_upsampled_tfidf, train_upsampled['label'], test_size = 0.2, random_state = 42)
-    # X_train_tfidf = train_upsampled_tfidf[y_train.index]
-    # X_val_tfidf = train_upsampled_tfidf[y_val.index]
-    #build the model
-    rf = RandomForestClassifier(n_estimators=400, random_state=11).fit(X_train_tfidf, y_train)
-    prediction = rf.predict(X_val_tfidf)
+    #Split train dataset
+    X_train, X_val, y_train, y_val = train_test_split(train_upsampled_tfidf, train_upsampled['label'], test_size = 0.2, random_state = 42)
+ 
+    #Build the model
+    nbtfidf = GaussianNB().fit(X_train, y_train) 
+    prediction = nbtfidf.predict(X_val)
     #print scores
-    f1 = f1_score(y_val, prediction)
-    print (f1)
-    # print(f"F1 score : {f1_score(y_val, prediction)}")
-    # print(f"Training Data Score: {rf.score(X_train_tfidf, y_train)}")
-    # print(f"Validation Data Score: {rf.score(X_val_tfidf, y_val)}")
-    # print(classification_report(y_val, prediction))
+    print(f"F1 score : {f1_score(y_val, prediction)}")
+    print(f"Training Data Score: {nbtfidf.score(X_train, y_train)}")
+    print(f"Validation Data Score: {nbtfidf.score(X_val, y_val)}")
+    print(classification_report(y_val, prediction))
+    
     #save model
-    model = 'humberto2.pkl'
-    joblib.dump(rf, open(model, 'wb'))
+    model = 'nbtfidf.pkl'
+    joblib.dump(nbtfidf, open(model, 'wb'))
 
     return
 
@@ -108,10 +88,10 @@ model_start()
 
 def tweet_predict (input1):
     #load model
-    model = joblib.load(open('humberto2.pkl', "rb"))
-    result = model.predict(input1.values())
+    model = joblib.load(open('nbtfidf.pkl', "rb"))
+    output = model.predict(input1.values())
 
-    return result[0]
+    return output[0]
 
 
 
